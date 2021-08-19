982418cbbbcd8d0042b49b1997bddd63fd27113e
SamBarrett1 <samuel.barrett@my.jcu.edu.au>
2021-08-19 16:26:14 +1000

    Recoded the temperature file with functions

--- prac_03/broken_score.py
+++ prac_03/broken_score.py
@@ -5,15 +5,36 @@ Samuel Barrett - 13038579
 Broken program to determine score status
 """
 
+import random
 
-score = float(input("Enter score: "))
-if score < 0 or score > 100:
-    print("Invalid score")
-elif score >= 90:
-    print("Excellent")
-elif score >= 50:
-    print("Passable")
-else:
-    print("Bad")
 
+def main():
+    """Program to return result of grades"""
+    score = float(input("Enter score: "))
+    result = get_result(score)
+    print(result)
+    # calling part c) the random grade function
+    random_grade = get_random_grade()
+    random_result = get_result(random_grade)
+    print("Random grade of {} is {}".format(random_grade, random_result))
+
+
+def get_result(grade):
+    """get result of grade"""
+    if grade < 0 or grade > 100:
+        return "Invalid score"
+    elif grade >= 90:
+        return "Excellent"
+    elif grade >= 50:
+        return "Passable"
+    else:
+        return "Bad"
+
+
+def get_random_grade():
+    random_grade = random.randint(0, 100)
+    return random_grade
+
+
+main()
 
