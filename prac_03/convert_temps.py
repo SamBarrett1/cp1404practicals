4a5ec730608d6760b006c71007a2c050e36f2cbb
SamBarrett1 <samuel.barrett@my.jcu.edu.au>
2021-08-19 18:59:35 +1000

    Add menu to convert temps file

--- prac_03/convert_temps.py
+++ prac_03/convert_temps.py
@@ -1,15 +1,34 @@
 """Program to read file and convert temperatures"""
 INFILE_NAME = 'temps_input.txt'
 OUTFILE_NAME = 'temps_output.txt'
+MENU = """C - Convert Celsius to Fahrenheit
+F - Convert Fahrenheit to Celsius"""
+# Q - Quit"""
 
 
 def main():
+    print(MENU)
+    choice = input(">>> ").upper()
     in_file = open(INFILE_NAME, 'r')
     out_file = open(OUTFILE_NAME, 'w')
-    for line in in_file:
-        temp = float(line)
-        celsius = fahrenheit_to_celsius(temp)
-        print(f"{temp:19} Fahrenheit is {celsius:19} Celsius", file=out_file)
+    # while choice != "Q":
+    if choice == "C":
+        for line in in_file:
+            temp = float(line)
+            celsius = fahrenheit_to_celsius(temp)
+            print(f"{temp:19} Fahrenheit is {celsius:19} Celsius", file=out_file)
+        # break
+    else:
+        for line in in_file:
+            temp = float(line)
+            fahrenheit = celsius_to_fahrenheit(temp)
+            print(f"{temp:19} Celsius is {fahrenheit:19} Fahrenheit", file=out_file)
+        # break
+        # else:
+        #     print("Invalid option")
+        #     print(MENU)
+        #     choice = input(">>> ").upper()
+    print("Temperature conversion is ready in your output file")
     in_file.close()
     out_file.close()
 
