380460eb5b3e947409426174ebe895024bb4bcb0
SamBarrett1 <samuel.barrett@my.jcu.edu.au>
2021-08-22 14:53:09 +1000

    Add practical 4 directory

---
+++ prac_04/lecture.py
@@ -0,0 +1,21 @@
+"""Wk 4 Practical - Samuel Barrett 13038579"""
+
+
+# user_name = input('Name: ').capitalize()
+# vowels = ['a', 'e', 'i', 'o', 'u']
+# cnt = 0
+# letter_cnt = len(user_name)
+# for i in vowels:
+#     if user_name.__contains__(i):
+#         cnt += 1
+# print('Out of {} letters, {} has {} vowels'.format(letter_cnt, user_name, cnt))
+
+user_name = input('Name: ').capitalize()
+vowels = 'aeiouAEIOU'
+cnt = 0
+letter_cnt = len(user_name)
+for letter in user_name:
+    if letter.lower() in vowels:
+        cnt += 1
+print('Out of {} letters, {} has {} vowels'.format(letter_cnt, user_name, cnt))
+
