88fb0409c2794e3ec958a08bde6cf61d8dfdd207
SamBarrett1 <samuel.barrett@my.jcu.edu.au>
2021-08-25 21:55:31 +1000

    Add woefull security checker

--- prac_04/list_exercises.py
+++ prac_04/list_exercises.py
@@ -6,6 +6,13 @@ NUMBERS = 5
 
 
 def main():
+    user_name = input("What is your name: ")
+    access_token = check_user_access(user_name)
+    if access_token:
+        print("Access granted")
+    else:
+        print("Access denied")
+        return
     user_numbers = []
     user_number_total = 0
     for i in range(1, NUMBERS + 1):
@@ -17,6 +24,13 @@ def main():
     print("The smallest number is {}".format(min(user_numbers)))
     print("The largest number is {}".format(max(user_numbers)))
     print("The average of the numbers is {:.1f}".format(user_number_total/NUMBERS))
+
+
+def check_user_access(name_string):
+    usernames = ['jimbo', 'giltson98', 'derekf', 'WhatSup', 'NicolEye', 'swei45', 'BaseInterpreterInterface',
+                 'BaseStdIn', 'Command', 'ExecState', 'InteractiveConsole', 'InterpreterInterface', 'StartServer',
+                 'bob']
+    return name_string in usernames
 
 
 main()
