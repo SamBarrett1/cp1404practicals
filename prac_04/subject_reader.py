741220f545cec59a0f6054c2b261edd442ba2cee
SamBarrett1 <samuel.barrett@my.jcu.edu.au>
2021-08-25 21:32:21 +1000

    Add practical 4 modifications

--- prac_04/subject_reader.py
+++ prac_04/subject_reader.py
@@ -14,6 +14,7 @@ def main():
 def get_data():
     """Read data from file formatted like: subject,lecturer,number of students."""
     input_file = open(FILENAME)
+    file_contents = []
     for line in input_file:
         print(line)  # See what a line looks like
         print(repr(line))  # See what a line really looks like
@@ -22,7 +23,11 @@ def get_data():
         print(parts)  # See what the parts look like (notice the integer is a string)
         parts[2] = int(parts[2])  # Make the number an integer (ignore PyCharm's warning)
         print(parts)  # See if that worked
+        print("{} is taught by {} and has {} students".format(parts[0], parts[1], parts[2]))
+        file_contents.append(parts)  # Append to file_contents list of lists
         print("----------")
+    print("List of lists:")
+    print(file_contents)  # See the list of lists
     input_file.close()
 
 
