b096d793e7b59867158c1a2131f04516e8aa8155
SamBarrett1 <samuel.barrett@my.jcu.edu.au>
2021-08-30 22:35:30 +1000

    add bug fix

--- prac_05/emails.py
+++ prac_05/emails.py
@@ -11,8 +11,8 @@ def main():
         user_name = get_name_from_email(user_email)
         is_name = input("Is your name {}. Y/n: ".format(user_name))
         if is_name.upper() != 'Y' and is_name != "":
-            is_name = input("Name: ")
-        email_to_name[user_email] = is_name
+            user_name = input("Name: ")
+        email_to_name[user_email] = user_name
         user_email = input("Email: ")
     for user_email, name in email_to_name.items():
         print("{} ({})".format(name, user_email))
