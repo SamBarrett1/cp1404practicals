4105027d6f92319b5fa17272650f1dc954e9dd72
SamBarrett1 <samuel.barrett@my.jcu.edu.au>
2021-09-18 22:35:33 +1000

    completed dynamic_labels.kv/py

--- prac_07/dynamic_labels.py
+++ prac_07/dynamic_labels.py
@@ -3,19 +3,16 @@
 from kivy.app import App
 from kivy.lang import Builder
 from kivy.uix.label import Label
-from kivy.properties import StringProperty
 
 __author__ = 'Samuel Barrett'
 
 
 class DynamicLabels(App):
     """Main program - Kivy dynamic labels application."""
-    status_text = StringProperty()
 
     def __init__(self, **kwargs):
         """Construct main app."""
         super().__init__(**kwargs)
-        # basic data example - dictionary of names: phone numbers
         self.names_list = ["Tony", "Therese", "Steve", "Jess", "Abraham", "Lauren"]
 
     def build(self):
@@ -32,19 +29,9 @@ class DynamicLabels(App):
             # add the label to the "main" layout widget
             self.root.ids.main.add_widget(temp_label)
 
-    def press_entry(self, instance):
-        """
-        Handle pressing entry buttons.
-        :param instance: the Kivy button instance that was clicked
-        """
-        # get name (dictionary key) from the text of Button we clicked on
-        name = instance.text
-        # update status text
-        self.status_text = "{}'s name is {}".format(name, self.names_list[name])
-
-    def clear_all(self):
-        """Clear all of the widgets that are children of the "entries_box" layout widget."""
-        self.root.ids.entries_box.clear_widgets()
+    # def clear_all(self):
+    #     """Clear all of the widgets that are children of the "entries_box" layout widget."""
+    #     self.root.ids.main.clear_widgets()
 
 
 DynamicLabels().run()
