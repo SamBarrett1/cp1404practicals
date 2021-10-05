9bd585146686c904f17f33e29548a46f999a4b17
SamBarrett1 <samuel.barrett@my.jcu.edu.au>
2021-10-05 21:11:58 +1000

    debugging taxi_simulator.py

--- prac_08/taxi_simulator.py
+++ prac_08/taxi_simulator.py
@@ -9,20 +9,14 @@ from prac_08.silver_service_taxi import SilverServiceTaxi
 
 def main():
     """Main program"""
-    fast_taxi = SilverServiceTaxi("FastCab", 100, 4)
-    flash_taxi = SilverServiceTaxi("FlashCab", 100, 6)
-    lux_taxi = SilverServiceTaxi("LuxCab", 100, 8)
 
-    taxi_objects = [fast_taxi, flash_taxi, lux_taxi]
+    taxi_objects = [SilverServiceTaxi("FastCab", 100, 4), SilverServiceTaxi("FlashCab", 100, 6),
+                    SilverServiceTaxi("LuxCab", 100, 8)]
 
     current_taxi = None
     total_cost = 0.00
 
     main_menu = ('q)uit,', 'c)hoose taxi,', 'd)rive')
-    taxi_menu = ('Choose your Taxi:\n'
-                 '1. FastCab\n'
-                 '2. FlashCab\n'
-                 '3. LuxCab')
 
     print("Let's drive!")
     print(main_menu)
@@ -30,42 +24,47 @@ def main():
 
     while choice != 'q':
         if choice == 'c':
-            while not current_taxi:
+            print('Choose your Taxi: ')
+            list_length = len(taxi_objects)
+            for i in range(0, list_length):
+                current_taxi = taxi_objects[i]
+                print("{} - {}".format(i, current_taxi))
+            taxi_choice = input(">>> ")
+            if taxi_choice == '0':
+                current_taxi = taxi_objects[int(taxi_choice)]
+            elif taxi_choice == '1':
+                current_taxi = taxi_objects[int(taxi_choice)]
+            elif taxi_choice == '2':
+                current_taxi = taxi_objects[int(taxi_choice)]
+            else:
+                print('Invalid taxi choice')
                 print('Choose your Taxi: ')
-                for taxi in enumerate(taxi_objects, start=0):
-                    print(taxi)
-                if choice == '0':
-                    current_taxi = taxi_objects[0]
-                    pass
-                elif choice == '1':
-                    current_taxi = taxi_objects[1]
-                    pass
-                elif choice == '2':
-                    current_taxi = taxi_objects[2]
-                    pass
-                else:
-                    print("Invalid option")
-                    print(taxi_menu)
-                    choice = input(">>> ")
-            current_fare = current_taxi.get_fare()
-            print("Bill to date: ${:.2f}".format(current_fare))
-            print(main_menu)
-            choice = input(">>> ").lower()
+                for i in range(0, list_length):
+                    current_taxi = taxi_objects[i]
+                    print("{} - {}".format(i, current_taxi))
+                taxi_choice = input(">>> ")
+
+            print('{} is your Taxi'.format(current_taxi.name))
+            print("Bill to date: ${:.2f}".format(current_taxi.get_fare()))
+            # print(main_menu)
+            # taxi_choice = input(">>> ").lower()
 
         elif choice == 'd':
-            if not current_taxi:
+            if current_taxi is None:
                 print('You need to choose a Taxi before you can drive')
                 print("Bill to date: ${:.2f}".format(total_cost))
-                print(main_menu)
-                choice = input(">>> ").lower()
             else:
                 print('Drive how far? ')
                 user_distance = input(">>> ").lower()
-                current_taxi.drive(user_distance)
-                pass
-                print('Your {} trip cost you {}'.format(current_taxi.name, current_taxi.get_fare()))
-                total_cost += current_taxi.get_fare()
-                print("Bill to date: ${:.2f}".format(total_cost))
+                if user_distance.isnumeric():
+                    current_taxi.drive(user_distance)
+                    print('Your {} trip cost you {}'.format(current_taxi.name, current_taxi.get_fare()))
+                    total_cost += current_taxi.get_fare()
+                    print("Bill to date: ${:.2f}".format(total_cost))
+                else:
+                    print('Invalid entry')
+                    print('Drive how far? ')
+                    user_distance = input(">>> ").lower()
             print(main_menu)
             choice = input(">>> ").lower()
         else:
@@ -73,7 +72,7 @@ def main():
             print(main_menu)
             choice = input(">>> ").lower()
 
-        print('Have a nice day')
+    print('Have a nice day')
 
 
 main()
