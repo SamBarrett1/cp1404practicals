b158c5136d19fb38e9d7176cdc6fc65939376c73
SamBarrett1 <samuel.barrett@my.jcu.edu.au>
2021-10-05 21:56:20 +1000

    completed taxi_simulator.py

--- prac_08/taxi_simulator.py
+++ prac_08/taxi_simulator.py
@@ -1,14 +1,12 @@
 """
 Practical 08 - Samuel Barrett - 13038579
-
-Program for driving Taxis using inheritance and polymorphism
 """
 
 from prac_08.silver_service_taxi import SilverServiceTaxi
 
 
 def main():
-    """Main program"""
+    """Program for driving Taxis using inheritance and polymorphism"""
 
     taxi_objects = [SilverServiceTaxi("FastCab", 100, 4), SilverServiceTaxi("FlashCab", 100, 6),
                     SilverServiceTaxi("LuxCab", 100, 8)]
@@ -29,49 +27,35 @@ def main():
             for i in range(0, list_length):
                 current_taxi = taxi_objects[i]
                 print("{} - {}".format(i, current_taxi))
-            taxi_choice = input(">>> ")
-            if taxi_choice == '0':
-                current_taxi = taxi_objects[int(taxi_choice)]
-            elif taxi_choice == '1':
-                current_taxi = taxi_objects[int(taxi_choice)]
-            elif taxi_choice == '2':
-                current_taxi = taxi_objects[int(taxi_choice)]
-            else:
+            taxi_choice = int(input(">>> "))
+            try:
+                current_taxi = taxi_objects[taxi_choice]
+            except IndexError:
                 print('Invalid taxi choice')
-                print('Choose your Taxi: ')
-                for i in range(0, list_length):
-                    current_taxi = taxi_objects[i]
-                    print("{} - {}".format(i, current_taxi))
-                taxi_choice = input(">>> ")
-
             print('{} is your Taxi'.format(current_taxi.name))
-            print("Bill to date: ${:.2f}".format(current_taxi.get_fare()))
-            # print(main_menu)
-            # taxi_choice = input(">>> ").lower()
 
         elif choice == 'd':
-            if current_taxi is None:
-                print('You need to choose a Taxi before you can drive')
-                print("Bill to date: ${:.2f}".format(total_cost))
-            else:
+            if current_taxi:
+                current_taxi.start_fare()
                 print('Drive how far? ')
-                user_distance = input(">>> ").lower()
-                if user_distance.isnumeric():
-                    current_taxi.drive(user_distance)
-                    print('Your {} trip cost you {}'.format(current_taxi.name, current_taxi.get_fare()))
-                    total_cost += current_taxi.get_fare()
-                    print("Bill to date: ${:.2f}".format(total_cost))
-                else:
-                    print('Invalid entry')
-                    print('Drive how far? ')
-                    user_distance = input(">>> ").lower()
-            print(main_menu)
-            choice = input(">>> ").lower()
+                user_distance = int(input(">>> "))
+                current_taxi.drive(user_distance)
+                print('Your {} trip cost you ${:.2f}'.format(current_taxi.name, current_taxi.get_fare()))
+            else:
+                print('You need to choose a Taxi before you can drive')
         else:
             print("Invalid option")
-            print(main_menu)
-            choice = input(">>> ").lower()
+        total_cost += current_taxi.get_fare()
+        print("Bill to date: ${:.2f}".format(total_cost))
+        print(main_menu)
+        choice = input(">>> ").lower()
 
+    print('Total trip cost: ${:.2f}'.format(total_cost))
+    print('Taxis are now')
+    list_length = len(taxi_objects)
+    for i in range(0, list_length):
+        current_taxi = taxi_objects[i]
+        print("{} - {}".format(i, current_taxi))
     print('Have a nice day')
 
 
